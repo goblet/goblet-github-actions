d7d537537935a513144b8f3bc8b4a27e8f71ee70
Brian <brian.lee@premise.com>
2021-09-27 14:59:07 -0400

    generalize to whole config

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,12 @@ if ! [ -z "$GOBLET_PATH" ]
 then
     cd "$GOBLET_PATH"
 fi
-config="{\"cloudfunction\":{\"environmentVariables\":{$1}}}"
-command="goblet deploy --project $PROJECT --location $LOCATION $stageSubCommand --config $config"
+
+if ! [ -z "$1" ]
+then
+    command="goblet deploy --project $PROJECT --location $LOCATION $stageSubCommand --config-from-json-string $1"
+else
+    command="goblet deploy --project $PROJECT --location $LOCATION $stageSubCommand"
+fi
 
 eval $command
