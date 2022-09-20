04c128c881cd95da87ae165266e483952cf123f7
Austen <anovis@ymail.com>
2022-09-20 11:32:55 -0400

    Goblet version (#16)
    * set goblet version
    
    * allow for goblet versions

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ RUN apt-get update
 
 RUN apt-get install -y git
 
-RUN pip install goblet-gcp
-
 COPY entrypoint.py /entrypoint.py
 
 ENTRYPOINT ["python3", "/entrypoint.py"]
--- entrypoint.py
+++ entrypoint.py
@@ -12,8 +12,17 @@ if __name__ == "__main__":
     envars = sys.argv[5]
     build_envars = sys.argv[6]
     custom_command = sys.argv[7]
+    goblet_version = sys.argv[8]
 
-
+    # install desired version og goblet
+    if goblet_version == "latest":
+        pip = subprocess.run(["pip", "install", "goblet-gcp"], capture_output=True)
+        if pip.returncode != 0:
+            raise Exception(pip.stderr)
+    else:
+        pip = subprocess.run(["pip", "install", f"goblet-gcp=={goblet_version}"], capture_output=True)
+        if pip.returncode != 0:
+            raise Exception(pip.stderr)
     os.chdir(goblet_path)
     pip = subprocess.run(["pip", "install", "-r", "requirements.txt"], capture_output=True)
     if pip.returncode != 0:
@@ -24,26 +33,28 @@ if __name__ == "__main__":
     if stage:
         stage_sub_command = f"--stage {stage}"
     if envars or build_envars:
-        envars_list = envars.split(',')
-        build_envars_list = build_envars.split(',')
         config = {
             "cloudfunction": {
                 "environmentVariables": {},
                 "buildEnvironmentVariables": {}
             }
         }
-        for envar in envars_list:
-            try:
-                key, value = envar.split(':', 1)
-                config["cloudfunction"]["environmentVariables"][key] = value
-            except ValueError:
-                raise "Environment variables are in the wrong format. Should be '{k1}:{v1},{k2}:{v2},...'"
-        for build_envar in build_envars_list:
-            try:
-                key, value = build_envar.split(':', 1)
-                config["cloudfunction"]["buildEnvironmentVariables"][key] = value
-            except ValueError:
-                raise "Build Environment variables are in the wrong format. Should be '{k1}:{v1},{k2}:{v2},...'"
+        if envars:
+            envars_list = envars.split(',')
+            for envar in envars_list:
+                try:
+                    key, value = envar.split(':', 1)
+                    config["cloudfunction"]["environmentVariables"][key] = value
+                except ValueError:
+                    raise "Environment variables are in the wrong format. Should be '{k1}:{v1},{k2}:{v2},...'"
+        if build_envars:
+            build_envars_list = build_envars.split(',')
+            for build_envar in build_envars_list:
+                try:
+                    key, value = build_envar.split(':', 1)
+                    config["cloudfunction"]["buildEnvironmentVariables"][key] = value
+                except ValueError:
+                    raise "Build Environment variables are in the wrong format. Should be '{k1}:{v1},{k2}:{v2},...'"
         config_sub_command = f"--config-from-json-string {json.dumps(config, separators=(',', ':'))}"
 
     if custom_command:
